9b022dcb7b50db4f21b3377190387dbf2d9edb84
AsenUzunov <transmit1988@abv.bg>
2022-12-31 11:13:54 +0200

    Change Login, Logout Model

--- TaskBoardApp/Controllers/UserController.cs
+++ TaskBoardApp/Controllers/UserController.cs
@@ -10,12 +10,17 @@ namespace TaskBoardApp.Controllers
     {
 
         private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
 
-        public UserController(UserManager<User> _userManager)
+        public UserController(
+            UserManager<User> _userManager, 
+            SignInManager<User> _signInManager)
         {
             this.userManager = _userManager;
+            this.signInManager = _signInManager;
         }
 
+        //Register
         [HttpGet]
         public IActionResult Register()
         {
@@ -27,6 +32,81 @@ namespace TaskBoardApp.Controllers
             var model = new RegisterViewModel();
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User()
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return View(model);
+        }
+
+        //Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null) 
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ TaskBoardApp/Models/User/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoardApp.Models.User
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        public string UserName { get; set; } = null!;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+
+
+    }
+}
