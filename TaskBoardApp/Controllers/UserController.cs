ab199a6bf3ad7f76aa0b13f75252f5e6badf31ee
AsenUzunov <transmit1988@abv.bg>
2022-12-31 10:34:02 +0200

    Change Register Model

---
+++ TaskBoardApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TaskBoardApp.Data.Entities;
+using TaskBoardApp.Models.User;
+
+namespace TaskBoardApp.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly UserManager<User> userManager;
+
+        public UserController(UserManager<User> _userManager)
+        {
+            this.userManager = _userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+    }
+}
